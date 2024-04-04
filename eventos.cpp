2c99b0ac2dab1c6275db61407497bb2df6435da0
tianequis <166029663+tianequis@users.noreply.github.com>
2024-04-04 18:30:02 +0000

    claseslistas

--- eventos.cpp
+++ eventos.cpp
@@ -3,6 +3,7 @@
 eventos::eventos(int duracion, std::string ubicacion, std::string tema){
     this -> duracion = duracion;
     this -> ubicacion = ubicacion;
-    this -> tema = tema;
+    this -> tema = tema; 
+    
     }
 ;
