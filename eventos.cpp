55ec83cf36c77034e12536f7904e75c7d41d821a
tianequis <166029663+tianequis@users.noreply.github.com>
2024-04-04 19:03:46 +0000

    bibliotecau

--- eventos.cpp
+++ eventos.cpp
@@ -4,6 +4,6 @@ eventos::eventos(int duracion, std::string ubicacion, std::string tema){
     this -> duracion = duracion;
     this -> ubicacion = ubicacion;
     this -> tema = tema; 
-    
+
     }
 ;
--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 #include<string.h>
 #include<stdlib.h>
 
-using namespace std;
+void menuInicio() {
+    std::cout << "!Bienvenido al Menu de Gestion¡" << std::endl;
+    std::cout << "-------------------------------" << std::endl;
+    std::cout << "1) Generar evento." << std::endl;
+    std::cout << "2) Registrar asistentes." << std::endl;
+    std::cout << "3) Consultar lista de asistentes." << std::endl;
+    std::cout << "4) Imprimir informe." << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "Ingrese su opcion: ";
+}
+
 
 int main(){
 
-
-
+    menuInicio();
     return 0;
 }
